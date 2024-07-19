e233c0b2358f6693ef74f7e55210e676a9a7dd55
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-07-19 12:39:06 +0200

    finished up to 2.17

--- MIT_SICP/SICP_chapter2.scm
+++ MIT_SICP/SICP_chapter2.scm
@@ -90,3 +90,127 @@
 
 
 ; 2.4
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+
+
+
+
+; 2.5
+(define (cons a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (car n)
+  (if (= (remainder n 2) 1)
+      0
+      (+ 1 (car (/ n 2)))))
+
+(define (cdr n)
+  (if (= (remainder n 3) 1)
+      0
+      (+ 1 (car (/ n 3)))))
+
+
+
+
+
+; 2.6
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+
+
+
+
+; 2.7, 2.8, 2.9, 2.10, 2.11 idk what it meansx, 2.12, 2.13
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (sub-interval x y)
+  (make-interval (if (> (lower-bound x) (lower-bound y))
+                     (- (lower-bound x) (lower-bound y))
+                     (- (lower-bound y) (lower-bound x)))
+                 (if (> (upper-bound x) (upper-bound y))
+                     (- (upper-bound x) (upper-bound y))
+                     (- (upper-bound y) (upper-bound x)))))
+
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval
+   x
+   (make-interval (if (= (upper-bound y) 0)
+                      0 ; display error some how
+                      (/ 1.0 (upper-bound y)))
+                  (if (= (lower-bound y) 0)
+                      0 ; display error some how
+                      (/ 1.0 (upper-bound y))))))
+
+(define (width-interval x)
+  (/ (- (upper-bound x) (lower-bound x)) 2))
+
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound interval)
+  (cdr interval))
+
+(define (lower-bound interval)
+  (car interval))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (make-center-percent c p)
+  (make-interval (- c (* c (/ p 100))) (+ c (* c (/ p 100)))))
+
+(define (percent i)
+  (* 100 (- (/ (upper-bound i) (center i)) 1)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (par1 r1 r2)
+  (div-interval (mul-interval r1 r2)
+                (add-interval r1 r2)))
+  
+(define (par2 r1 r2)
+  (let ((one (make-interval 1 1)))
+    (div-interval
+     one (add-interval (div-interval one r1)
+                       (div-interval one r2)))))
+
+(define f (make-center-percent 2 5))
+(define s (make-center-percent 3 4))
+
+(par1 f f)
+(par2 f f)
+
+
+
+
+
+; 2.17
+(define (last-pair l)
+  (if (null? (cdr l))
+      (car l)
+      (last-pair (cdr l))))
+
+
+
+
+
+; 
