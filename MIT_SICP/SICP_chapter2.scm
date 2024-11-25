a7b7c458b1b8b55e125d9cc33eeb3d22bbd19d6e
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-11-24 21:59:02 -0500

    2.53 to 2.57

--- MIT_SICP/SICP_chapter2.scm
+++ MIT_SICP/SICP_chapter2.scm
@@ -557,5 +557,117 @@
 
 
 
-; i'm too lazy to do 2.42 and 2.43
-; 2.44
+; skipping picture language section
+
+; 2.53
+(list 'a 'b 'c)                           ; (a b c)
+(list (list 'george))                     ; ((george))
+(cdr '((x1 x2) (y1 y2)))                  ; ((y1 y2))
+(cadr '((x1 x2) (y1 y2)))                 ; (y1 y2)
+(pair? (car '(a short list)))             ; #f
+(memq 'red '((red shoes) (blue socks)))   ; #f
+(memq 'red '(red shoes blue socks))       ; (red shoes blue socks)
+
+
+
+
+
+; 2.54
+(define (my_equal l1 l2)
+  (cond ((and (null? l1) (null? l2)) #t)
+        ((or (null? l1) (null? l2)) #f)
+        ((not (eq? (car l1) (car l2))) #f)
+        (else (my_equal (cdr l1) (cdr l2)))))
+
+
+
+
+
+; 2.55
+; (car ''abracadabra) returns quote since the interpreter saw the second
+; quote as a symbol
+
+
+
+; deriv code
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                              (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type: DERIV" exp))))
+
+
+
+
+
+; 2.56
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                              (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        ((exponent? exp)
+         (make-product (exponent  exp)
+                       (make-exponent var (- (exponent exp) 1))))
+        (else
+         (error "unknown expression type: DERIV" exp))))
+
+
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+(define (exponent? x) (and (pair? x) (eq? (car x) '**)))
+(define (variable? x) (symbol? x))
+(define (=number? exp num) (and (number? exp) (= exp num)))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (make-exponent e1 e2)
+  (cond ((=number? e2 0) 1)
+        ((=number? e2 1) e1)
+        ((and (number? e1) (number? e2)) (exp e1 e2))
+        (else (list '** e1 e2))))
+        
+(define (exp a b)
+  (if (zero? b) 1
+      (* a (exp a (- b 1)))))
+
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+(define (multiplier p) (cadr p))
+(define (multiplicand p) (caddr p))
+(define (base e) (cadr e))
+(define (exponent e) (caddr e))
+
+
+
+
+
+; 2.57
